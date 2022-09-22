1d5a1bc8cef5b838205e713232caa2525e1c7287
TonyCitterio <tcitterio@hotmail.com>
2022-09-22 16:19:10 +0200

    done so far

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/general/Navbar";
+import Home from "./components/views/Home";
 
 function App() {
   return (
     <>
     <Navbar />
+    <Home />
     </>
   );
 }
