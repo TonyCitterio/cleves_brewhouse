f50b6a900ddd7f4bb88f7b9e1e20222a98021bb1
TonyCitterio <tcitterio@hotmail.com>
2022-09-27 15:58:06 +0200

    made some changes

--- src/components/general/Navbar.js
+++ src/components/general/Navbar.js
@@ -3,24 +3,26 @@ import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <section>
+    <section className={classes.navbar}>
+      <div>
         <h1 className={classes.heading}>
           Cleves
-          <span className={classes.center}>Brew</span>
+          <span>Brew</span>
           <span>House</span>
         </h1>
-      </section>
-      <section className={classes.link}>
+      </div>
+      <div className={classes.link}>
         <p>Hem</p>
         <p>Om mig</p>
         <p>Min utrustning</p>
         <p>Mina öl</p>
-      </section>
-      <section className={classes.contact}>
-        <a href="mailto:tcitterio@hotmail.com"><IoMail className={classes.icon}/></a>
-      </section>
-    </nav>
+      </div>
+      <div className={classes.contact}>
+        <a href="mailto:tcitterio@hotmail.com">
+          <IoMail className={classes.icon} />
+        </a>
+      </div>
+    </section>
   );
 };
 
