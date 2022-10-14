47908904d0836353d38e84a65f2ee46126e98179
TonyCitterio <tcitterio@hotmail.com>
2022-10-14 15:22:28 +0200

    working with pictures

--- src/components/views/Brewery.js
+++ src/components/views/Brewery.js
@@ -1,12 +1,13 @@
 import classes from "./Brewery.module.css"
+import background from "../pictures/pexels-cottonbro-5537952.jpg"
 
 const Brewery = () => {
     return (
         <>
         <div className={classes.breweryContent}>
-
         <h1>Bryggeriet</h1>
         </div>
+        <div className={classes.test} style={{backgroundImage: `url(${background})`, backgroundSize:`cover`, backgroundPosition: `center`}}></div>
         </>
     );
 }
