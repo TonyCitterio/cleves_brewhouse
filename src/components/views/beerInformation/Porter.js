93ecbb52f54917655332e80a8f6dcc5451adf883
TonyCitterio <tcitterio@hotmail.com>
2022-10-25 16:42:07 +0200

    done with this

--- src/components/views/beerInformation/Porter.js
+++ src/components/views/beerInformation/Porter.js
@@ -1,41 +1,80 @@
 import { Link } from "react-router-dom";
 import classes from "./Porter.module.css";
-import beerLabel from "../../pictures/lottas-dubbelpils-600px.png"
-import background from "../../pictures/dencleceskoldskalagern.jpeg"
+import beerLabel from "../../pictures/etikett milkshake.jpg";
+import background from "../../pictures/dencleceskoldskalagern.jpeg";
 
 const Porter = () => {
   return (
     <>
       <div className={classes.porterMain}>
         <div className={classes.porterLink}>
-            <Link className={classes.porterLinkStyle} to="/mybeer">Tillbaka</Link>
+          <Link className={classes.porterLinkStyle} to="/mybeer">
+            Tillbaka
+          </Link>
         </div>
         <div className={classes.porterContent}>
-          <div className={classes.porterIntro}> 
-            <img src={beerLabel} alt="beer label" height={200} width={200}></img>
+          <div className={classes.porterIntro}>
             <div>
-            <h1>Cleves Porter</h1>
-            <p>Ett öl med specialhumle som ger en lyxig smak.</p>
+              <img
+                src={beerLabel}
+                alt="beer label"
+                height={180}
+                width={180}
+              ></img>
+            </div>
+            <div>
+              <div>
+                <h1>Cleves Ipa</h1>
+                <p>
+                  Ett öl bryggd med mjölksocker som har en söt ton av
+                  jordgubbar, melon och en hint av ananas.
+                </p>
+              </div>
             </div>
           </div>
           <div className={classes.porterInfo}>
-            <div style={{
+            <div
+              style={{
                 backgroundImage: `url( ${background} )`,
                 backgroundSize: `cover`,
                 backgroundPosition: `center`,
-              }}></div>
+              }}
+            ></div>
             <div>
-            <h2>Information om ölen</h2>
+              <h2>Information om ölen</h2>
               <ul>
-                <li><span>Färg:</span>Mycket klar, mellanmörk med gyllene rött inslag.</li>
-                <li><span>Doft:</span> Tydlig ton av sött mörkt bröd med inslag av apelsin/mandarin.</li>
-                <li><span>Smak:</span>Tydlig fyllig maltkropp med klar sötma, låg beska och smak av pomeransskal.</li>
-                <li><span>Malt:</span>Wienermalt 65%, Carahell 31%, Cararoma 4%.</li>
-                <li><span>Humle:</span>Magnum, Saaz & Tettnanger i kok - torrhumlad med Mandarin Bavaria.</li>
-                <li><span>Jäsning & lagring:</span>Underjäst lager i 12 dagar på 12gr. Kyld i 38 dagar.</li>
-                <li><span>Stamvörtstyrka:</span>(Sockerhalt före jäsning) 14,6%</li>
-                <li><span>Alkohol %:</span>5,6</li>
-                <li><span>Restextrakt:</span>4,3%</li>
+                <li>
+                  <span>Färg:</span>Mycket klar, mellanmörk med gyllene rött
+                  inslag.
+                </li>
+                <li>
+                  <span>Doft:</span> Tydlig ton av sött mörkt bröd med inslag av
+                  apelsin/mandarin.
+                </li>
+                <li>
+                  <span>Smak:</span>Tydlig fyllig maltkropp med klar sötma, låg
+                  beska och smak av pomeransskal.
+                </li>
+                <li>
+                  <span>Malt:</span>Wienermalt 65%, Carahell 31%, Cararoma 4%.
+                </li>
+                <li>
+                  <span>Humle:</span>Magnum, Saaz & Tettnanger i kok -
+                  torrhumlad med Mandarin Bavaria.
+                </li>
+                <li>
+                  <span>Jäsning & lagring:</span>Underjäst lager i 12 dagar på
+                  12gr. Kyld i 38 dagar.
+                </li>
+                <li>
+                  <span>Stamvörtstyrka:</span>(Sockerhalt före jäsning) 14,6%
+                </li>
+                <li>
+                  <span>Alkohol %:</span>5,6
+                </li>
+                <li>
+                  <span>Restextrakt:</span>4,3%
+                </li>
               </ul>
             </div>
           </div>
--- src/components/views/beerInformation/SourAle.js
+++ src/components/views/beerInformation/SourAle.js
@@ -1,47 +1,79 @@
 import { Link } from "react-router-dom";
 import classes from "./SourAle.module.css";
-import beerLabel from "../../pictures/lottas-bjorkstadens-special-irish-red.png";
-import background from "../../pictures/clevestomte-ishPorter.jpeg"
+import beerLabel from "../../pictures/etikett sur.jpg";
+import background from "../../pictures/clevestomte-ishPorter.jpeg";
 
 const SourAle = () => {
   return (
     <>
       <div className={classes.sourAleMain}>
-          <div className={classes.sourAleLink}>
-
-            <Link className={classes.sourAleLinkStyle} to="/mybeer">Tillbaka</Link>
-          </div>
+        <div className={classes.sourAleLink}>
+          <Link className={classes.sourAleLinkStyle} to="/mybeer">
+            Tillbaka
+          </Link>
+        </div>
         <div className={classes.sourAleContent}>
           <div className={classes.sourAleIntro}>
-          <img
-              src={beerLabel}
-              alt="beer label"
-              height={200}
-              width={200}
-            ></img>
             <div>
-            <h1>Cleves speciella suröl</h1>
-            <p>Här kommer en sur en. Kazbek humlen ger en citruslik fräschör till detta speciella öl</p>
+              <img
+                src={beerLabel}
+                alt="beer label"
+                height={180}
+                width={180}
+              ></img>
+            </div>
+            <div>
+              <div>
+                <h1>Cleves speciella Surmust</h1>
+                <p>
+                  En öl gjord på en traditionell bas av malt men med ett
+                  tillskott av koncentrerad julmust. Ölen får sin sura smak{" "}
+                  <br></br> av en jäst som producerar mjölksyra.
+                </p>
+              </div>
             </div>
           </div>
           <div className={classes.sourAleInfo}>
-            <div style={{
+            <div
+              style={{
                 backgroundImage: `url( ${background} )`,
                 backgroundSize: `cover`,
                 backgroundPosition: `center`,
-              }}></div>
-            <div><h2>Information om ölen</h2>
-            <ul>
-              <li><span>Färg:</span>Halmgul med vitt fast skum.</li>
-              <li><span>Doft:</span>Ton av örter och citrus.</li>
-              <li><span>Smak:</span> Frisk och stram smak med inslag av citrus och markerad beska.</li>
-              <li><span>Malt:</span>Wiener malt 100%.</li>
-              <li><span>Humle:</span>Magnum & Saaz, torrhumlad med Kazbek.</li>
-              <li><span>Jäsning & lagringrg:</span>Underjäst pils, jästid 8 dagar, kyl stabiliserad i 25 dagar.</li>
-              <li><span>Stamvörtstyrka:</span>14,59</li>
-              <li><span>Alkohol %:</span>5.8</li>
-              <li><span>Restextrakt:</span>3.7%</li>
-            </ul>
+              }}
+            ></div>
+            <div>
+              <h2>Information om ölen</h2>
+              <ul>
+                <li>
+                  <span>Färg:</span>Halmgul med vitt fast skum.
+                </li>
+                <li>
+                  <span>Doft:</span>Ton av örter och citrus.
+                </li>
+                <li>
+                  <span>Smak:</span> Frisk och stram smak med inslag av citrus
+                  och markerad beska.
+                </li>
+                <li>
+                  <span>Malt:</span>Wiener malt 100%.
+                </li>
+                <li>
+                  <span>Humle:</span>Magnum & Saaz, torrhumlad med Kazbek.
+                </li>
+                <li>
+                  <span>Jäsning & lagringrg:</span>Underjäst pils, jästid 8
+                  dagar, kyl stabiliserad i 25 dagar.
+                </li>
+                <li>
+                  <span>Stamvörtstyrka:</span>14,59
+                </li>
+                <li>
+                  <span>Alkohol %:</span>5.8
+                </li>
+                <li>
+                  <span>Restextrakt:</span>3.7%
+                </li>
+              </ul>
             </div>
           </div>
         </div>
